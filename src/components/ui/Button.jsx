1c196c23d9d6c171b0c69d08ff42a1cb500178df
angx88 <angx89@gmail.com>
2026-06-07 01:26:23 +0700

    Tambah komponen Button

---
+++ src/components/ui/Button.jsx
@@ -0,0 +1,16 @@
+﻿export default function Button({
+  children,
+  className = "",
+  type = "button",
+  ...props
+}) {
+  return (
+    <button
+      type={type}
+      className={`rounded-xl px-4 py-2 text-sm font-black transition active:scale-[0.98] disabled:cursor-not-allowed disabled:opacity-60 ${className}`}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+}
